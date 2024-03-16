ddb3599f3d31c646c3628181bcccc9af9af4e99c
Graciete Gil <graciete.gil@gmail.com>
2024-03-16 18:54:22 +0000

    Update urls and views

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-
-from .views import Index
+from .views import Index, my_recipes, healthy_recipes, add_recipe, profile
 
 urlpatterns = [
     path('', Index.as_view(), name='home'),
+    path('my_recipes/', my_recipes, name='my_recipes'),
+    path('healthy_recipes/', healthy_recipes, name='healthy_recipes'),
+    path('add_recipe/', add_recipe, name='add_recipe'),
+    path('profile/<int:user_id>', profile, name='profile'),
+    # other url patterns...
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,20 @@
-from django.views.generic import TemplateView   
-
-# Create your views here.
+from django.shortcuts import render
+from django.views.generic import TemplateView
 
 class Index(TemplateView):
     template_name = 'home/index.html'
+
+def my_recipes(request):
+    # your view logic here...
+    return render(request, 'my_recipes.html')
+
+def healthy_recipes(request):
+    # your view logic here...
+    return render(request, 'healthy_recipes.html')
+
+def add_recipe(request):
+    # your view logic here...
+    return render(request, 'add_recipe.html')
+def profile(request,user_id):
+    # your view logic here...
+    return render(request, 'profile.html')
