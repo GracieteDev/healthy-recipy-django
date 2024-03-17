e872d2f024344dfceb20fdf94857f34d70a118d0
Graciete Gil <graciete.gil@gmail.com>
2024-03-17 04:41:39 +0000

    create templates for healthy_recipes app

--- healthy_recipes/models.py
+++ healthy_recipes/models.py
@@ -34,15 +34,24 @@ class Recipe(models.Model):
     ingredients = SummernoteTextField(max_length=10000, null=False, blank=False)
     image = CloudinaryField('image')
     image_alt = models.CharField(max_length=100, null=False, blank=False)
+    
+    #dropdown menus
     meal_type = models.CharField(max_length=50, choices=MEAL_TYPES, default="breakfast")
     cuisine_types = models.CharField(
         max_length=50, choices=CUISINE_TYPES, default="african"
     )
-    calories = models.IntegerField()
-    posted_date = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        ordering = ["-posted_date"]
+    servings = models.CharField(
+        max_length=100, null=False, blank=False)
+    posted_date = models.DateTimeField(
+        auto_now=True)
+    freezable = models.BooleanField(
+        default=False)
+    favourites = models.ManyToManyField(
+        User, related_name='favourite', default=None, blank=True)
+    
 
     def __str__(self):
         return str(self.title)
+
+    class Meta:
+        ordering = ["-posted_date"]
