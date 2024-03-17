1b72dfe55f762f66dc12518a128216d8052aa211
Graciete Gil <graciete.gil@gmail.com>
2024-03-17 03:53:20 +0000

    profiles model migration

--- healthy_recipes/models.py
+++ healthy_recipes/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django_summernote.fields import SummernoteTextField
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+# Choice Fields
+MEAL_TYPES = (("breakfast", "Breakfast"), ("lunch", "Lunch"), ("dinner", "Dinner"))
+
+CUISINE_TYPES = (
+    ("african", "African"),
+    ("american", "American"),
+    ("caribbean", "Caribbean"),
+    ("asian", "Asian"),
+    ("middle_eastern", "Middle Eastern"),
+    ("chinese", "Chinese"),
+    ("indian", "Indian"),
+    ("pakistani", "Pakistani"),
+    ("indonesian", "Indonesian"),
+    ("european", "European"),
+    ("oceanic", "Oceanic"),
+)
+
+class Recipe(models.Model):
+    """
+    A model to create and manage recipes
+    """
+
+    user = models.ForeignKey(
+        User, related_name="recipe_owner", on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=300, null=False, blank=False)
+    description = models.CharField(max_length=500, null=False, blank=False)
+    instructions = SummernoteTextField(max_length=10000, null=False, blank=False)
+    ingredients = SummernoteTextField(max_length=10000, null=False, blank=False)
+    image = CloudinaryField('image')
+    image_alt = models.CharField(max_length=100, null=False, blank=False)
+    meal_type = models.CharField(max_length=50, choices=MEAL_TYPES, default="breakfast")
+    cuisine_types = models.CharField(
+        max_length=50, choices=CUISINE_TYPES, default="african"
+    )
+    calories = models.IntegerField()
+    posted_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-posted_date"]
+
+    def __str__(self):
+        return str(self.title)
--- profiles/models.py
+++ profiles/models.py
@@ -1,8 +1,23 @@
 from django.db import models
+from django_summernote.fields import SummernoteTextField
+from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
-
-# Create your models here.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # other fields...
+    """Profile model"""
+
+    user = models.ForeignKey(User, related_name="profile", on_delete=models.CASCADE)
+    image = CloudinaryField('image', default='https://res.cloudinary.com/your-cloud-name/image/upload/v1621234567/default.jpg')
+    bio = SummernoteTextField(max_length=2500, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.user.username)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(instance, created, **kwargs):
+    """Create or update the user profile"""
+    if created:
+        Profile.objects.create(user=instance)
